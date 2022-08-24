8a96ac04c180189932c5e4a2fd790ebdfdbc1fff
Deepak tiwari <atrivedi8988@gmail.com>
2022-08-24 17:05:42 +0530

    skeleton

--- ideakart-app/src/App.js
+++ ideakart-app/src/App.js
@@ -1,10 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import { AllRoute } from './Routes/AllRoute';
+import { Navbar } from './Components/Navbar';
 
 function App() {
   return (
     <div className="App">
-      <h1>Idea-Kart-WebSite</h1>
+      
+      <Navbar/>
+      <AllRoute/>
+
+      {/* <h1>Idea-Kart-WebSite</h1> */}
     </div>
   );
 }
