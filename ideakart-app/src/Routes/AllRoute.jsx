0fe59a4d7dd57a7fbb5bcd00080da01f9d3b2263
Deepak tiwari <atrivedi8988@gmail.com>
2022-08-25 22:55:45 +0530

    about page complete

--- ideakart-app/src/Routes/AllRoute.jsx
+++ ideakart-app/src/Routes/AllRoute.jsx
@@ -1,11 +1,12 @@
 import {Routes,Route} from "react-router-dom"
+import { About } from "./About"
 import { Home } from "./Home"
 
 export function AllRoute(){
     return (
         <Routes>
             <Route path="/" element={<Home/>}  />
-            <Route path="/about" element={<h1>About Page</h1>}  />
+            <Route path="/about" element={<About/>}  />
             <Route path="/contact" element={<h1>Contact Page</h1>}  />
             <Route path="/sign_in" element={<h1>SignIn Page</h1>}  />
             <Route path="/sign_up" element={<h1>SignUp Page</h1>}  />
