b5b3e95114f1aa0314ad85f3fac47b1174847ec9
Deepak tiwari <atrivedi8988@gmail.com>
2022-08-26 06:56:51 +0530

    signup and signin and about is complete

--- ideakart-app/src/Routes/AllRoute.jsx
+++ ideakart-app/src/Routes/AllRoute.jsx
@@ -1,6 +1,8 @@
 import {Routes,Route} from "react-router-dom"
 import { About } from "./About"
 import { Home } from "./Home"
+import { SignIn } from "./SignIn"
+import { SignUp } from "./SignUp"
 
 export function AllRoute(){
     return (
@@ -8,8 +10,8 @@ export function AllRoute(){
             <Route path="/" element={<Home/>}  />
             <Route path="/about" element={<About/>}  />
             <Route path="/contact" element={<h1>Contact Page</h1>}  />
-            <Route path="/sign_in" element={<h1>SignIn Page</h1>}  />
-            <Route path="/sign_up" element={<h1>SignUp Page</h1>}  />
+            <Route path="/sign_in" element={<SignIn/>}  />
+            <Route path="/sign_up" element={<SignUp/>}  />
         </Routes>
     )
 }
